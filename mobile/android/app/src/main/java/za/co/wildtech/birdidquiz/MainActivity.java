37b694dff6fdfbe495ac574079386230810b508a
Nicholas Cook <cooknh@gmail.com>
2026-01-01 19:21:34 +0200

    feat: privacy policy

--- mobile/android/app/src/main/java/za/co/wildtech/birdidquiz/MainActivity.java
+++ mobile/android/app/src/main/java/za/co/wildtech/birdidquiz/MainActivity.java
@@ -1,5 +1,15 @@
 package za.co.wildtech.birdidquiz;
 
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition for Android 12+
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        
+        super.onCreate(savedInstanceState);
+    }
+}
